44179456e4c6c1b88595a1a40f5beff7869b0c8c
Christopher Casey <chris.casey1234@gmail.com>
2023-07-03 15:00:47 -0700

    adds function to open dynamic rep and res menus

--- pages/exercise/[_id].js
+++ pages/exercise/[_id].js
@@ -39,7 +39,6 @@ export default function ExerciseId({params, myExercise}) {
   useEffect(() => {
     // Function to call when the component mounts
     getExercise();
-    console.log(exercise)
 
   }, []);
 
@@ -55,13 +54,70 @@ export default function ExerciseId({params, myExercise}) {
       });
   }
 
+  const [resMenuOpen, setResMenuOpen] = useState({
+    resOneMenu : false,
+    resTwoMenu : false,
+    resThreeMenu : false,
+    repOneMenu : false,
+    repTwoMenu : false,
+    repThreeMenu : false
+  })
 
+
+  function handleClick (e) {
+    // Create a new copy of the state object
+    const newState = { ...resMenuOpen };
+    let menuName = e.target.name + "Menu"
+    console.log(menuName)
+    // Modify the desired property in the new state object
+    newState[menuName] = !newState[menuName];
+
+    for (let key in newState) {
+      if (key !== menuName) {
+        newState[key] = false
+      }
+    }
+    // Update the state with the new state object
+    setResMenuOpen(newState)
+
+    console.log(resMenuOpen)
+  }
 
   return (
-    <div className="flex flex-col justify-center items-center mr-2 w-full">
+      <div className="w-full flex flex-col">
+        <div className="flex flex-row justify-start mt-60 ml-5">
+          <h1 className="text-3xl text-bold">{exercise[0].name}</h1>
+        </div>
+        <h3 className="flex flex-row justify-center text-2xl">Resistance</h3>
+        <div className="flex flex-row justify-center m-5 mt-10">
+          <div className="flex flex-col w-1/3 justify-center">
+            <button className="border border-r-0 border-gray-300 px-6 py-4 rounded-sm text-xl w-full" name="resOne" onClick={handleClick}>None</button>
+            {resMenuOpen.resOneMenu && <div>resOne</div>}
+          </div>
+          <div className="flex flex-col w-1/3 justify-center">
+            <button className="border border-r-0 border-gray-300 px-6 py-4 rounded-l text-xl w-full"  name="resTwo" onClick={handleClick}>Weights</button>
+            {resMenuOpen.resTwoMenu && <div>resTwo</div>}
+          </div>
+          <div className="flex flex-col w-1/3 justify-center">
+            <button className="border border-gray-300 px-6 py-4 rounded-r text-xl w-full"  name="resThree" onClick={handleClick}>Bands</button>
+            {resMenuOpen.resThreeMenu && <div>resThree</div>}
+          </div>
+        </div>
+        <div className="flex flex-row justify-center m-5 mt-10">
+          <div className="flex flex-col w-1/3 justify-center">
+              <button className="border border-r-0 border-gray-300 px-6 py-4 rounded-sm text-xl w-full" name="repOne" onClick={handleClick}>None</button>
+              {resMenuOpen.repOneMenu && <div>repOne</div>}
+            </div>
+            <div className="flex flex-col w-1/3 justify-center">
+              <button className="border border-r-0 border-gray-300 px-6 py-4 rounded-l text-xl w-full"  name="repTwo" onClick={handleClick}>Weights</button>
+              {resMenuOpen.repTwoMenu && <div>repTwo</div>}
+            </div>
+            <div className="flex flex-col w-1/3 justify-center">
+              <button className="border border-gray-300 px-6 py-4 rounded-r text-xl w-full"  name="repThree" onClick={handleClick}>Bands</button>
+              {resMenuOpen.repThreeMenu && <div>repThree</div>}
+            </div>
+        </div>
+      </div>
 
-      <h1>{exercise[0].name}</h1>
-
-    </div>
     )
   }
